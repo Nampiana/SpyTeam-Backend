a95a3585946a1c96a4c62f8a8b674b03f62ad87f
Nampiana <89507479+Nampiana@users.noreply.github.com>
2025-03-25 16:36:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set("trust proxy", (ip) => {
 // Configurer CORS
 app.use(
   cors({
-    origin: ["https://localhost:3000"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
